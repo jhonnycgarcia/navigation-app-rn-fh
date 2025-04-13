a27dd23bf057561fe70a646a599f29ff54ce25fb
Jhonny <jhonnycgarcia@gmail.com>
2025-04-13 00:56:03 -0500

    useSafeAreaInserts

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,9 +1,26 @@
 import { View, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PrimaryButton } from '../../components';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
 
 export const ProfileScreen = () => {
+
+  const { top } = useSafeAreaInsets();
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 20,
+        marginTop: top + 20,
+      }}
+    >
+      <Text style={{ marginBottom: 10 }}>ProfileScreen</Text>
+      <PrimaryButton
+        label="Open Drawer"
+        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+      />
     </View>
   );
 };
