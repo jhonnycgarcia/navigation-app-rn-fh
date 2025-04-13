a9a754e9e92ed701909132113a96980852413f68
Jhonny <jhonnycgarcia@gmail.com>
2025-04-13 00:10:48 -0500

    Stack - PopToTop

--- src/presentation/screens/setting/SettingScreen.tsx
+++ src/presentation/screens/setting/SettingScreen.tsx
@@ -1,9 +1,22 @@
 import { View, Text } from 'react-native';
+import { globalStyles } from '../../theme/theme';
+import { PrimaryButton } from '../../components';
+import { type NavigationProp, StackActions, useNavigation } from '@react-navigation/native';
+import { type RootStackParamList } from '../../routes/StackNavigator';
 
 export const SettingScreen = () => {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
   return (
-    <View>
-      <Text>SettingScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={{ marginBottom: 10, fontSize: 30 }}>SettingScreen</Text>
+      <PrimaryButton
+        label="Go Back"
+        onPress={() => navigation.goBack()}
+      />
+      <PrimaryButton
+        label="Go to Home"
+        onPress={() => navigation.dispatch(StackActions.popToTop())}
+      />
     </View>
   );
 };
