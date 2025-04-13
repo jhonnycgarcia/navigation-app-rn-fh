d9d1532d1303b621085da49abfb26da074dad5b0
Jhonny <jhonnycgarcia@gmail.com>
2025-04-13 00:51:46 -0500

    drawer personalizado

--- src/presentation/routes/SideMenuNavigator.tsx
+++ src/presentation/routes/SideMenuNavigator.tsx
@@ -1,14 +1,53 @@
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Text, useWindowDimensions, View } from 'react-native';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, type DrawerContentComponentProps } from '@react-navigation/drawer';
+
 import { StackNavigator } from './StackNavigator';
 import { ProfileScreen } from '../screens';
+import { globalColors } from '../theme/theme';
+
 
 const Drawer = createDrawerNavigator();
 
 export const SideMenuNavigator = () => {
+
+    const dimensions = useWindowDimensions();
+
     return (
-        <Drawer.Navigator>
+        <Drawer.Navigator
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+            screenOptions={{
+                headerShown: false,
+                drawerType: dimensions.width >= 768 ? 'permanent' : 'slide',
+                drawerActiveBackgroundColor: globalColors.primary,
+                drawerActiveTintColor: 'white',
+                drawerInactiveTintColor: globalColors.primary,
+                drawerItemStyle: {
+                    borderRadius: 100,
+                    paddingHorizontal: 20,
+                },
+            }}
+        >
           <Drawer.Screen name="StackNavigator" component={StackNavigator} />
           <Drawer.Screen name="Profile" component={ProfileScreen} />
         </Drawer.Navigator>
       );
+};
+
+const CustomDrawerContent = (props: DrawerContentComponentProps) => {
+    return (
+        <DrawerContentScrollView {...props}>
+            <View
+                style={{
+                    height: 200,
+                    backgroundColor: globalColors.primary,
+                    margin: 30,
+                    borderRadius: 50,
+                }}
+            />
+
+            <DrawerItemList {...props} />
+
+            <Text>Version 1.0.0</Text>
+        </DrawerContentScrollView>
+    );
 };
