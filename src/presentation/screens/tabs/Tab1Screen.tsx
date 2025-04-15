a49489265d0dc4200aa12fd5aa15ac5f72c2ded2
Jhonny <jhonnycgarcia@gmail.com>
2025-04-14 22:23:15 -0500

    menu de hamburguesa

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,8 +1,10 @@
 import { View, Text } from 'react-native';
+import { HamburgerMenu } from '../../components/shared/HamburgerMenu';
 
 export const Tab1Screen = () => {
   return (
     <View>
+      <HamburgerMenu />
       <Text>Tab1Screen</Text>
     </View>
   );
